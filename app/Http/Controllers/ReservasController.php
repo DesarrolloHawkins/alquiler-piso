c3cb2e8957befb11f4499555001cd345ee66db6b
DesarrolloHawkins <desarrollo.hawkins@gmail.com>
2023-10-25 21:38:29 +0200

    Mejoras del controlador de reservas

--- app/Http/Controllers/ReservasController.php
+++ app/Http/Controllers/ReservasController.php
@@ -70,25 +70,32 @@ class ReservasController extends Controller
 
     public function agregarReserva(Request $request){
 
-
+        // Obtenemos la Fecha de Hoy
         $hoy = Carbon::now();
+        // Declaramos Variables
         $cliente;
         $reserva;
+        
         // Convertimos las Request en la data
         $data = $request->all();
         // Almacenamos la peticion en un archivo
         Storage::disk('local')->put($data['codigo_reserva'].'-' . $hoy .'.txt', json_encode($request->all()));
+
         // Comprobamos si la reserva ya existe
         $comprobarReserva = Reserva::where('codigo_reserva', $data['codigo_reserva'])->first();
         // Si la reserva no existe procedemos al registro
         if ($comprobarReserva == null) {
-            $verificarCliente = Cliente::where('telefono',$data['telefono'] )->first();
+            // Obtenemos el Cliente si existe por el numero de telefono
+            $verificarCliente = Cliente::where('telefono', $data['telefono'] )->first();
+            // Validamos si existe el cliente
             if ($verificarCliente == null) {
+                // Si no existe separamos el nombre y el numero de personas para el apartamento
 				if (preg_match('/^(.*?)\n(\d+)\s*adulto(?:s)?/', $data['alias'], $matches)) {
+                    // Establecemos el nombre y numero de adultos
 					$nombre = trim($matches[1]);
 					$num_adultos = $matches[2];
 
-
+                    // Creamos el cliente
 					$crearCliente = Cliente::create([
 						'alias' => $nombre,
 						'idiomas' => $data['idiomas'],
@@ -98,6 +105,7 @@ class ReservasController extends Controller
 					$cliente = $crearCliente;
 					
 				}else {
+                    // Si existe creamos al cliente
 					$crearCliente = Cliente::create([
 						'alias' => $data['alias'],
 						'idiomas' => $data['idiomas'],
@@ -109,56 +117,56 @@ class ReservasController extends Controller
 				
 				
             }else {
+                // En caso que el cliente ya existe
                 $cliente = $verificarCliente;
             }
-            $locale = 'es'; // Establece el idioma a español para reconocer 'jue' como 'jueves' y 'sep' como 'septiembre'
-
+            // Establece el idioma a español para reconocer 'jue' como 'jueves' y 'sep' como 'septiembre'
+            $locale = 'es';
 			Carbon::setLocale($locale);
+            // Parseamos las Fechas
            	$fecha_entrada = Carbon::createFromFormat('Y-m-d', $data['fecha_entrada']);
 			$fecha_salida = Carbon::createFromFormat('Y-m-d', $data['fecha_salida']);
 
-
-
-			//return $fecha_salida[1];
-
-            //$apartamento = Apartamento::where('id_booking', $data['codigo_reserva'])->first();
-
+            // Verificamos si la reserva existe por el codigo de reserva
             $verificarReserva = Reserva::where('codigo_reserva',$data['codigo_reserva'] )->first();
-            if ($data['origen'] == 'Booking') {
-                $apartamento = Apartamento::where('id_booking', $data['apartamento'])->first();
-                # code...
-            }else if($data['origen'] == 'Airbnb'){
-                switch ($data['apartamento']) {
-                    case 'Ático nuevo en pleno centro. Plaza alta.':
-                        $apartamento = ['id'=> 1];
-                        break;
-                    case 'Apartamento interior Algeciras 2a':
-                        $apartamento = ['id'=> 2];
-                        break;
-                    case 'Apartamento en el absoluto centro 2b':
-                        $apartamento = ['id'=> 3];
-                        break;
-                    case 'Apartamento interior centro en Algeciras 1º A':
-                        $apartamento = ['id'=> 4];
-                        break;
-                    case 'Apartamento en absoluto centro. 1ºB':
-                        $apartamento = ['id'=> 5];
-                        break;
-                    case 'Apartamento en absoluto centro. ba':
-                        $apartamento = ['id'=> 6];
-                        break;
-                    case 'ÁApartamento BB Centro Algeciras':
-                        $apartamento = ['id'=> 7];
-                        break;
-
-                    
-                    default:
-                        $apartamento = ['id'=> null];
-                        break;
-                }
-            }
-
+            // Si la reserva no existe
             if ($verificarReserva == null) {
+                // Comprobamos el origen para obtener el ID del apartamento
+                if ($data['origen'] == 'Booking') {
+                    // Si es booking lo obtenemos por el id del apartamento en booking
+                    $apartamento = Apartamento::where('id_booking', $data['apartamento'])->first();
+                }else if($data['origen'] == 'Airbnb'){
+                    // Si es de Airbnb lo obtenemos por el nombre del apartamento
+                    switch ($data['apartamento']) {
+                        case 'Ático nuevo en pleno centro. Plaza alta.':
+                            $apartamento = ['id'=> 1];
+                            break;
+                        case 'Apartamento interior Algeciras 2a':
+                            $apartamento = ['id'=> 2];
+                            break;
+                        case 'Apartamento en el absoluto centro 2b':
+                            $apartamento = ['id'=> 3];
+                            break;
+                        case 'Apartamento interior centro en Algeciras 1º A':
+                            $apartamento = ['id'=> 4];
+                            break;
+                        case 'Apartamento en absoluto centro. 1ºB':
+                            $apartamento = ['id'=> 5];
+                            break;
+                        case 'Apartamento en absoluto centro. ba':
+                            $apartamento = ['id'=> 6];
+                            break;
+                        case 'ÁApartamento BB Centro Algeciras':
+                            $apartamento = ['id'=> 7];
+                            break;
+    
+                        
+                        default:
+                            $apartamento = ['id'=> null];
+                            break;
+                    }
+                }
+                // Creamos la Reserva
                 $crearReserva = Reserva::create([
                     'codigo_reserva' => $data['codigo_reserva'],
                     'origen' => $data['origen'],
@@ -171,36 +179,49 @@ class ReservasController extends Controller
     
                 ]);
                 $reserva = $crearReserva;
+                return response('Registrado', 200);
 
             } else {
+                // Si ya existe la reserva
                 $reserva = $verificarReserva;
+                return response('Ya existe la Reserva', 200);
 
             }
             
-            return response('Registrado');
         } else {
-            return response('Ya existe esa reserva');
+            return response('Ya existe la Reserva', 200);
         }
 
     }
 
     public function verificarReserva(Request $request){
-		return 'ok';
-        $data = $request->all();
+		//return 'ok';
 
+        // Obtenemos los dato de la Reserva
+        $data = $request->all();
+        // Comprobamos si existe
         $reserva = Reserva::where('codigo_reserva', $data['codigo_reserva'])->first();
 
         if ($reserva != null) {
-            return response(true);
+            return response('La reserva existe', 404);
         }
         
-        return response(false);
+        return response('No existe la reserva', 200);
     }
 	
 	public function cancelarAirBnb($reserva){
+        // Conprobamos la reserva con el codigo de reserva
 		$reserva = Reserva::where('codigo_reserva', $reserva)->first();
+        // Si la reserva no existe
+        if ($reserva== null) {
+            return response('La reserva no existe', 404);
+        }
+        // Si la reserva existe
+        // Cambiamos el estado a CAncelado
 		$reserva->estado_id = 4;
 		$reserva->save();
+        
+        return response('La reserva de ha cancelado', 200);
 		
 	}
 }
