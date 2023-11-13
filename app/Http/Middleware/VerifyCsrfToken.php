91c844d7c9ffb9aebf40c82afcf60c7551af020e
DesarrolloHawkins <desarrollo.hawkins@gmail.com>
2023-11-13 14:06:58 +0100

    permisos comprobar reserva

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,6 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         '/agregar-reserva',
         '/comprobar-reserva/*',
-		'/verificar-reserva',
+		'/verificar-reserva/*',
     ];
 }
