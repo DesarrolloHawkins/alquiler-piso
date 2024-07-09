05205ff9f322bcad805f68e7d746e2490ed5134f
DesarrolloHawkins <122017573+DesarrolloHawkins@users.noreply.github.com>
2024-07-09 12:43:33 +0200

    asds

--- app/Http/Controllers/WhatsappController.php
+++ app/Http/Controllers/WhatsappController.php
@@ -784,6 +784,7 @@ class WhatsappController extends Controller
         // Ejecutar la solicitud y obtener la respuesta
         $response = curl_exec($curl);
         curl_close($curl);
+        Storage::disk('local')->put('REspuestaImagenChat.txt', json_encode($response));
 
         // Procesar la respuesta
         if ($response === false) {
