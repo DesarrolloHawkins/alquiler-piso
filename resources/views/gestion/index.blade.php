8c5512bd156e11855ab837ff6c2cbc9897a4c8d7
DesarrolloHawkins <122017573+DesarrolloHawkins@users.noreply.github.com>
2024-08-28 15:11:58 +0200

    correcion gestion index

--- resources/views/gestion/index.blade.php
+++ resources/views/gestion/index.blade.php
@@ -152,7 +152,7 @@
                                             <a data-id="{{$reservaLimpieza->id}}" class=" list-group-item d-flex justify-content-between align-items-start">
                                                 <div class="ms-2 me-auto">
                                                     <div class="fw-bold">{{$reservaLimpieza->id}} - {{$reservaLimpieza->apartamento->nombre}}</div>
-                                                    Fecha Salida: {{$reservaLimpieza->origenReserva->fecha_salida}}
+                                                    Fecha Salida: @if(isset($reservaLimpieza->origenReserva->fecha_salida)) {{$reservaLimpieza->origenReserva->fecha_salida}} @endif
                                                 </div>
                                             </a>
     
