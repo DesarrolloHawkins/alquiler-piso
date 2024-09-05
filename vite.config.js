435f3af8d244ee70bc2710d1f3172aca27c2e8e1
DesarrolloHawkins <122017573+DesarrolloHawkins@users.noreply.github.com>
2024-09-05 17:01:52 +0200

    vite configuracion

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/calendar.js'],
-
-            refresh: true,
-        }),
-    ],
-    server: {
-        hmr: {
-          overlay: false,
-        },
-      },
+  plugins: [
+    laravel({
+      input: ['resources/js/app.js', 'resources/sass/app.scss'], // Asegúrate de incluir app.scss aquí
+      refresh: true,
+    }),
+  ],
 });
