ed7df2b2c529175e7a2e1a5ef246a2e77c4fc1bb
DesarrolloHawkins <122017573+DesarrolloHawkins@users.noreply.github.com>
2024-09-06 11:33:02 +0200

    correciones tabla reservas

--- resources/views/admin/reservas/tabla.blade.php
+++ resources/views/admin/reservas/tabla.blade.php
@@ -10,89 +10,102 @@
     .active-sort {
         color: #757191;
     }
+    body { font-family: Arial, sans-serif; }
+    table { width: 100%; border-collapse: collapse; white-space: nowrap; }
+    th, td { border: 1px solid #ccc; padding: 8px; text-align: center; }
+    th { background-color: #f2f2f2; }
+    .header { background-color: #4CAF50; color: white; padding: 10px; }
+
+    /* Evitar salto de línea en la columna de apartamentos */
+    .apartments-column {
+        white-space: nowrap;
+        width: auto;
+    }
+
+    /* Establecer el scroll horizontal */
+    .table-responsive {
+        overflow-x: auto;
+    }
 </style>
-<style>
-  body { font-family: Arial, sans-serif; }
-  table { width: 100%; border-collapse: collapse; }
-  th, td { border: 1px solid #ccc; padding: 8px; text-align: center; }
-  th { background-color: #f2f2f2; }
-  .header { background-color: #4CAF50; color: white; padding: 10px; }
-</style>
+
 <div class="container-fluid">
     <div class="d-flex flex-colum mb-3">
         <h2 class="mb-0 me-3 encabezado_top">{{ __('Tabla de Reservas') }}</h2>
     </div>
     <hr class="mb-5">
     <div class="row justify-content-center">
-      <div class="header">
-        <a href="{{ route('admin.tablaReservas.index', ['date' => \Carbon\Carbon::createFromFormat('Y-m', $date)->subMonth()->format('Y-m')]) }}" class="nav-link">Mes Anterior</a>
-        <h1>Calendario de Reservas para {{ $monthName }}</h1>
-        <a href="{{ route('admin.tablaReservas.index', ['date' => \Carbon\Carbon::createFromFormat('Y-m', $date)->addMonth()->format('Y-m')]) }}" class="nav-link">Mes Siguiente</a>
-      </div>
-      @if ($apartamentos)
-        <table class="table table-bordered">
-          <thead>
-              <tr>
-                  <th>Apartamentos</th>
-                  @for ($day = 1; $day <= $daysInMonth; $day++)
-                      <th>{{ $day }}</th>
-                  @endfor
-              </tr>
-          </thead>
-          <tbody>
-              @foreach ($apartamentos as $apartamento)
-                  <tr>
-                      <td>{{ $apartamento->titulo }}</td>
-                      
-                      @for ($day = 1; $day <= $daysInMonth; $day++)
-                      @php
-                          $found = false;
-                      @endphp
-                  
-                      {{-- Buscar si hay una reserva que coincida con este día --}}
-                      @foreach ($apartamento->reservas as $itemReserva)
-                          @if ($itemReserva->fecha_entrada->day == $day)
-                              @php
-                                  // Comparar la fecha con la fecha de hoy
-                                  $fechaHoy = \Carbon\Carbon::now(); // Obtener la fecha actual
-                  
-                                  // Obtener la fecha del día de la reserva en el mismo formato
-                                  $fechaReserva = \Carbon\Carbon::parse($itemReserva->fecha_entrada);
-                  
-                                  $claseBoton = '';
-                  
-                                  // Comparar si la fecha de entrada de la reserva es pasada, hoy o futura
-                                  if ($fechaReserva->isPast() && !$fechaReserva->isToday()) {
-                                      $claseBoton = 'btn-warning'; // Pasado
-                                  } elseif ($fechaReserva->isToday()) {
-                                      $claseBoton = 'btn-success'; // Hoy (verde)
-                                  } else {
-                                      $claseBoton = 'btn-info'; // Futuro
-                                  }
-                              @endphp
-                              <td>
-                                  <button type="button" class="btn {{ $claseBoton }}" data-bs-toggle="modal" data-bs-target="#modalReserva{{ $itemReserva->id }}">
-                                      ({{ $itemReserva->fecha_entrada->format('d/m') }})
-                                  </button>
-                              </td>
-                              @php
-                                  $found = true;
-                              @endphp
-                              @break  {{-- Salir del bucle de reservas si ya encontramos una para este día --}}
-                          @endif
-                      @endforeach
-                  
-                      {{-- Si no se encontró ninguna reserva, agregar una celda vacía --}}
-                      @if (!$found)
-                          <td></td>
-                      @endif
-                  @endfor
-                  
-                  </tr>
-              @endforeach
-          </tbody>
-        </table>
-      
+        <div class="header">
+            <a href="{{ route('admin.tablaReservas.index', ['date' => \Carbon\Carbon::createFromFormat('Y-m', $date)->subMonth()->format('Y-m')]) }}" class="nav-link">Mes Anterior</a>
+            <h1>Calendario de Reservas para {{ $monthName }}</h1>
+            <a href="{{ route('admin.tablaReservas.index', ['date' => \Carbon\Carbon::createFromFormat('Y-m', $date)->addMonth()->format('Y-m')]) }}" class="nav-link">Mes Siguiente</a>
+        </div>
+
+        @if ($apartamentos)
+        <!-- Contenedor con scroll horizontal -->
+        <div class="table-responsive">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th class="apartments-column">Apartamentos</th>
+                        @for ($day = 1; $day <= $daysInMonth; $day++)
+                            <th>{{ $day }}</th>
+                        @endfor
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($apartamentos as $apartamento)
+                        <tr>
+                            <td class="apartments-column">{{ $apartamento->titulo }}</td>
+                            
+                            @for ($day = 1; $day <= $daysInMonth; $day++)
+                                @php
+                                    $found = false;
+                                @endphp
+                            
+                                {{-- Buscar si hay una reserva que coincida con este día --}}
+                                @foreach ($apartamento->reservas as $itemReserva)
+                                    @if ($itemReserva->fecha_entrada->day == $day)
+                                        @php
+                                            // Comparar la fecha con la fecha de hoy
+                                            $fechaHoy = \Carbon\Carbon::now(); // Obtener la fecha actual
+                            
+                                            // Obtener la fecha del día de la reserva en el mismo formato
+                                            $fechaReserva = \Carbon\Carbon::parse($itemReserva->fecha_entrada);
+                            
+                                            $claseBoton = '';
+                            
+                                            // Comparar si la fecha de entrada de la reserva es pasada, hoy o futura
+                                            if ($fechaReserva->isPast() && !$fechaReserva->isToday()) {
+                                                $claseBoton = 'btn-warning'; // Pasado
+                                            } elseif ($fechaReserva->isToday()) {
+                                                $claseBoton = 'btn-success'; // Hoy (verde)
+                                            } else {
+                                                $claseBoton = 'btn-info'; // Futuro
+                                            }
+                                        @endphp
+                                        <td>
+                                            <button type="button" class="rounded-0 btn {{ $claseBoton }}" data-bs-toggle="modal" data-bs-target="#modalReserva{{ $itemReserva->id }}">
+                                                ({{ $itemReserva->fecha_entrada->format('d') }})
+                                            </button>
+                                        </td>
+                                        @php
+                                            $found = true;
+                                        @endphp
+                                        @break  {{-- Salir del bucle de reservas si ya encontramos una para este día --}}
+                                    @endif
+                                @endforeach
+                            
+                                {{-- Si no se encontró ninguna reserva, agregar una celda vacía --}}
+                                @if (!$found)
+                                    <td></td>
+                                @endif
+                            @endfor
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        
         @foreach ($apartamentos as $apartamento)
             @foreach ($apartamento->reservas as $itemReserva)
               <!-- Modal -->
