4fed4f0667bf12ce698699b2819ea20d44269d47
DesarrolloHawkins <122017573+DesarrolloHawkins@users.noreply.github.com>
2024-09-10 11:45:16 +0200

    modificar migracion

--- database/migrations/2024_09_09_204501_invoice_reference_autoincrement.php
+++ database/migrations/2024_09_09_204501_invoice_reference_autoincrement.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('invoices_reference', function (Blueprint $table) {
+        Schema::create('invoices_reference', function (Blueprint $table) {
             $table->id();
             
             $table->string('reference_autoincrement')->nullable();
