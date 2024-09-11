af6f5d48aae83637fafba228c9f2d586f0821f78
DesarrolloHawkins <122017573+DesarrolloHawkins@users.noreply.github.com>
2024-09-11 09:01:46 +0200

    Revert "correcion"
    This reverts commit a87a0846d13f4875856d2718053438babc4950f7.

--- app/Http/Controllers/WhatsappController.php
+++ app/Http/Controllers/WhatsappController.php
@@ -215,7 +215,6 @@ class WhatsappController extends Controller
         $id = $data['entry'][0]['changes'][0]['value']['messages'][0]['id'];
         $phone = $data['entry'][0]['changes'][0]['value']['messages'][0]['from'];
         $mensaje = $data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body'];
-        $id_thread = $data['entry'][0]['changes'][0]['value']['messages'][0]['id'];
 
         $mensajeExiste = ChatGpt::where('id_mensaje', $id)->get();
 
@@ -225,7 +224,7 @@ class WhatsappController extends Controller
 
             $dataRegistrar = [
                 'id_mensaje' => $id,
-                'id_three' => $id_thread,
+                'id_three' => $mensajeExiste->id_three,
                 'remitente' => $phone,
                 'mensaje' => $mensaje,
                 'respuesta' => null,
