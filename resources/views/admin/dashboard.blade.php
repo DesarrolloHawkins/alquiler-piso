d06d5997cf054b30a448f3d39a9fc7251db66bd5
DesarrolloHawkins <122017573+DesarrolloHawkins@users.noreply.github.com>
2024-09-12 09:55:10 +0200

    subir para ver meses

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -55,5 +55,41 @@
         </div>
     </div>
 
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 mt-4">
+                <form action="{{ route('dashboard.index') }}" method="GET">
+                    <label for="mes">Selecciona un mes:</label>
+                    <select name="mes" id="mes" class="form-control">
+                        <option value="1">Enero</option>
+                        <option value="2">Febrero</option>
+                        <option value="3">Marzo</option>
+                        <option value="4">Abril</option>
+                        <option value="5">Mayo</option>
+                        <option value="6">Junio</option>
+                        <option value="7">Julio</option>
+                        <option value="8">Agosto</option>
+                        <option value="9">Septiembre</option>
+                        <option value="10">Octubre</option>
+                        <option value="11">Noviembre</option>
+                        <option value="12">Diciembre</option>
+                    </select>
+                    <button type="submit" class="btn btn-primary mt-3 w-100">Consultar</button>
+                </form>
+                <div class="container mt-4">
+                    <h4 class="text-center">Reporte de Reservas para el Año {{ $anio }} @if($mes) y Mes {{ $mes }} @endif</h4>
+                    <hr>
+
+                    <p class="mb-0 text-center fs-5">Total de Reservas:</p>
+                    <p class="fs-4 mb-0 text-center"><strong>{{ $countReservas }}</strong></p>
+                    <p class="mb-0 text-center fs-5">Total de Ingresos:</p>
+                    <p class="fs-4 mb-0 text-center"><strong>{{ number_format($sumPrecio, 2) }} €</strong></p>
+
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    
 </div>
 @endsection
