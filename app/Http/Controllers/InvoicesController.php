325427efb33d961df51a4195817b050577e62319
DesarrolloHawkins <122017573+DesarrolloHawkins@users.noreply.github.com>
2024-10-01 11:10:22 +0200

    concepto dentro de invocice

--- app/Http/Controllers/InvoicesController.php
+++ app/Http/Controllers/InvoicesController.php
@@ -95,6 +95,7 @@ class InvoicesController extends Controller
             'invoice' => $invoice,
         ];
         $conceptos = Reserva::where('id',$invoice->reserva_id)->get();
+        $invoice['conceptos'] = $conceptos;
         // dd($conceptos);
         // Sanitizar el nombre del archivo para eliminar caracteres no válidos
         $fileName = 'factura_' . preg_replace('/[^A-Za-z0-9_\-]/', '', $invoice->reference) . '.pdf';
