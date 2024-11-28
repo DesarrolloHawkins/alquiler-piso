bdd46c8fae954bcff40ba14e5d4008c7f20df85f
DesarrolloHawkins <122017573+DesarrolloHawkins@users.noreply.github.com>
2024-11-28 16:00:50 +0100

    Update ChannexController.php

--- app/Http/Controllers/ChannexController.php
+++ app/Http/Controllers/ChannexController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Carbon\Carbon;
 
 class ChannexController extends Controller
 {
     public function webhook(Request $request){
-        Storage::disk('publico')->put('Channex-WebHook.txt', json_encode($request->all()));
+        $fecha = Carbon::now()->format('Y-m-d_H-i-s'); // Puedes cambiar el formato según lo que necesites
+
+        Storage::disk('publico')->put("Channex-WebHook_{$fecha}.txt", json_encode($request->all()));
 
         return response()->json('Enviado correctamente', 200);
     }
