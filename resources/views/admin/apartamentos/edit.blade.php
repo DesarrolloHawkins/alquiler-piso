96d6434a23fbd3e88e4878f7b3599bdbff5e6695
DesarrolloHawkins <122017573+DesarrolloHawkins@users.noreply.github.com>
2025-02-20 09:15:52 +0100

    Update edit.blade.php

--- resources/views/admin/apartamentos/edit.blade.php
+++ resources/views/admin/apartamentos/edit.blade.php
@@ -217,7 +217,7 @@
 
                 <!-- Información Importante -->
                 <div class="col-sm-12 col-md-6 mb-3">
-                    <label for="claves" class="form-label">Información Importante</label>
+                    <label for="claves" class="form-label">Claves de Acceso</label>
                     <textarea class="form-control @error('claves') is-invalid @enderror" id="claves" name="claves" placeholder="Claves para acceder al apartamento">{{ old('claves', $apartamento->claves) }}</textarea>
                     @error('claves')
                         <div class="invalid-feedback">{{ $message }}</div>
