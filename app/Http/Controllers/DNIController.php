19fdebb146d47acf4f32acbbbced24d79adb66fa
HawkinsWeb <128600888+crmhawkins@users.noreply.github.com>
2025-08-25 14:43:30 +0200

    idioma dni error

--- app/Http/Controllers/DNIController.php
+++ app/Http/Controllers/DNIController.php
@@ -628,7 +628,7 @@ class DNIController extends Controller
         ];
 
         //dd($data);
-        return view('dni.index', compact('id', 'paises', 'reserva', 'data', 'textos','paisCliente','paisesDni', 'optionesTipo', 'cliente'));
+        return view('dni.index', compact('id', 'paises', 'reserva', 'data', 'textos','paisCliente','paisesDni', 'optionesTipo'));
     }
 
     public function listadoPaises(){
