1b6df94dce5c7d00ce2d1f3205e6ba9b9f86473f
HawkinsWeb <128600888+crmhawkins@users.noreply.github.com>
2025-10-28 13:23:29 +0100

    Update CalendarioController.php

--- app/Http/Controllers/CalendarioController.php
+++ app/Http/Controllers/CalendarioController.php
@@ -14,7 +14,7 @@ class CalendarioController extends Controller
     // Solo enviamos reservas de OTAs, NO reservas web ni presenciales
     $reservas = Reserva::where('apartamento_id', $id)
         ->whereNotIn('estado_id', [4, 7])
-        ->whereNotIn('origen', ['Web', 'Presencial'])
+        ->whereNotIn('origen', ['Web'])
         ->get();
 
     $calendario = "BEGIN:VCALENDAR\r\n";
