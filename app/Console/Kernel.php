da484fa4bb238762cf3f123116ee196b7e6164c3
HawkinsWeb <128600888+crmhawkins@users.noreply.github.com>
2026-01-12 17:04:17 +0100

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -351,8 +351,8 @@ class Kernel extends ConsoleKernel
         })->everyMinute();
 
         // Ejecutar el comando cada hora
-        $schedule->command('ari:fullsync')->hourly();
-        $schedule->command('ari:liberar-canceladas')->everyFiveMinutes();
+       // $schedule->command('ari:fullsync')->hourly();
+        //$schedule->command('ari:liberar-canceladas')->everyFiveMinutes();
 
         // Tarea par enviar los mensajes automatizados cuando se ha entregado el DNI
         $schedule->call(function (ClienteService $clienteService) {
